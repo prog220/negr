e016460460d1c0b488f0155bae5b3c5a68a555e6
prog220 <146746245+prog220@users.noreply.github.com>
2025-02-19 20:44:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var i = document.getElementById('bu')
+var h = document.getElementById('he')
+var l = document.getElementById('l')
+
+function press() {
+    i.src = "img/img.jpg"
+    h.innerHTML = "LOX"
+    l.innerHTML = "why did you press it you're stupid moron"
+}
